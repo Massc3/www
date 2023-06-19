11950a0544ece1cf9c3e793b21c9c0962dd43e00
MassC3 <massoutar.c@gmail.com>
2023-06-19 16:55:38 +0200

    Coucou

--- CaiampitimMassoutar/AlgoPHP/Partie1/exoo12.php
+++ CaiampitimMassoutar/AlgoPHP/Partie1/exoo12.php
@@ -1,7 +1,18 @@
 <h1>Exercice 12</h1>
 
-<p>
-
+<p>A partir d’une fonction personnalisée et à partir d’un tableau de prénoms et de langue associée 
+(tableau contenant clé et valeur), dire bonjour aux différentes personnes dans leur langue 
+respective (français ➔ « Salut », anglais ➔ « Hello », espagnol ➔ « Hola »)
+Exemple : tableau ➔ Mickaël -> FRA, Virgile -> ESP, Marie-Claire -> ENG
+Affichage :
+Salut Mickaël
+Hola Virgile
+Hello Marie-Claire
+Variante : trier d’abord le tableau par ordre alphabétique du prénom
+Affichage :
+Hello Marie-Claire
+Salut Mickaël
+Hola Virgile
 </p>
 
 <?php
